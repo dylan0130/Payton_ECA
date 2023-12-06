9807673a86ad697f41341f1b4d2b186b55aa37d8
Dylan/Payton <144731984+dylan0130@users.noreply.github.com>
2023-12-06 11:40:49 -0700

    Update indexScript.js

--- indexScript.js
+++ indexScript.js
@@ -1,6 +1,12 @@
 let nameOfElement;
 nameOfElement = "demo";
 
+// Código en español
+alert("Welcome pookie");
+
+// English code
+console.log("hi pookies");
+
 function changeText(){
   document.getElementById(nameOfElement).innerHTML = "lol";
   console.log(nameOfElement);
